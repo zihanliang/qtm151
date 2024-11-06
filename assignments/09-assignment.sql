ed5002bd3bbec5532cc5ddf78f3f5997f3d2222d
Zihan Liang <165616540+zihanliang@users.noreply.github.com>
2024-11-06 16:48:16 -0500

    Assignment 9

--- assignments/09-assignment.sql
+++ assignments/09-assignment.sql
@@ -1,7 +1,7 @@
 -- QTM 151 - Assignment 09
 -- Add your name here (use -- to comment)
--- Name:
--- Emory Id:
+-- Name: Zihan Liang
+-- Emory Id: 2609381
 
 -- Using pdAdmin or any other SQL client, run the following SQL script to create a new table.
 -- Then answer the questions in the assignment.
@@ -28,23 +28,43 @@ INSERT INTO drivers (driver_id, driver_name, nationality, lap_times) VALUES
 (10, 'Michael Holzmieter', 'German', 24);
 
 -- Question 1: Write an SQL query to select all columns from the drivers table.
+SELECT * FROM drivers;
 
 -- Question 2: Write an SQL query to retrieve only the driver_name and nationality of all drivers.
+SELECT driver_name, nationality FROM drivers;
 
 -- Question 3: Write an SQL query to find all drivers with the nationality 'German'.
+SELECT * FROM drivers
+WHERE nationality = 'German';
 
 -- Question 4: Write an SQL query to select drivers who have completed more than 50 lap times.
+SELECT * FROM drivers
+WHERE lap_times > 50;
 
 -- Question 5: Write an SQL query to count the number of drivers from each nationality.
+SELECT nationality, COUNT(*) AS driver_count
+FROM drivers
+GROUP BY nationality;
 
 -- Question 6: Write an SQL query to find the driver with the highest number of lap times.
+SELECT * FROM drivers
+ORDER BY lap_times DESC
+LIMIT 1;
 
 -- Question 7: Write an SQL query to list all drivers ordered by their lap times in descending order.
+SELECT * FROM drivers
+ORDER BY lap_times DESC;
 
 -- Question 8: Write an SQL statement to insert a new driver with driver_id 11, driver_name 'Jenson Button', nationality 'British', and lap_times 45.
+INSERT INTO drivers (driver_id, driver_name, nationality, lap_times)
+VALUES (11, 'Jenson Button', 'British', 45);
 
 -- Question 9: Write an SQL query to calculate the average number of lap times of all drivers.
+SELECT AVG(lap_times) AS average_lap_times
+FROM drivers;
 
--- Question 10: Question 13: Write an SQL query to find drivers whose lap times are between 20 and 80.
+-- Question 10: Write an SQL query to find drivers whose lap times are between 20 and 80.
+SELECT * FROM drivers
+WHERE lap_times BETWEEN 20 AND 80;
 
 -- Submit the SQL queries for the questions above.
